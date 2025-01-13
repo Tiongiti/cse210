441434b74e2dc410ed74cceb31fd90f3763e11b8
Tiongiti <ttioti2011@gmail.com>
2025-01-13 21:08:10 +1400

    finally it work

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,14 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Hello World! This is the Exercise1 Project.");
-        string first = Console.ReadLine()
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
 
-
-        Console.Write("What is your first name");
+        Console.Write("What is your last name? ");
         string last = Console.ReadLine();
 
-
-        Console.WriteLine($"Your name is{last},{first},{last}.");
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
